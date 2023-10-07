b30b8ddacd9e6e1c265d1a87559768823761b271
Vojtech Mares <iam@vojtechmares.com>
2023-10-07 09:04:29 +0200

    feat(vojtechmares.com): eslint rules: add next/core-web-vitals

--- apps/vojtechmares.com/.eslintrc.js
+++ apps/vojtechmares.com/.eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  extends: ["next", "prettier"],
+  extends: ["next", "prettier", "next/core-web-vitals"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
