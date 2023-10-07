7ebe33a52a4cf8b1e32701e56ba752a192f7e9f4
Vojtech Mares <iam@vojtechmares.com>
2023-10-07 11:20:52 +0200

    fix(vojtechmares.com): add app/ to tailwind.config.js

--- apps/vojtechmares.com/tailwind.config.js
+++ apps/vojtechmares.com/tailwind.config.js
@@ -1,12 +1,11 @@
 module.exports = {
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
+    "./app/**/*.{ts,tsx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
   ],
   theme: {
-    extend: {}
+    extend: {},
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ]
-}
+  plugins: [require("@tailwindcss/typography")],
+};
