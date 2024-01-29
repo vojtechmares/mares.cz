8eeb1b5f04ff8ffcf9527a2b2087e132c1b2949b
Vojtech Mares <iam@vojtechmares.com>
2024-01-29 21:14:13 +0100

    style: apply prettier format

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,6 @@ export const viewport: Viewport = {
   initialScale: 1.0,
 };
 
-
 export const metadata: Metadata = {
   title: "Vojtěch Mareš - DevOps konzultant, lektor, engineer",
   description: "Vojtěch Mareš - DevOps konzultant, lektor, engineer",
