22b27b990507e62dc172b5c173a5d9ea54baee42
Vojtech Mares <iam@vojtechmares.com>
2024-06-02 08:55:21 +0200

    refactor(footer): replace Twitter with X

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -126,16 +126,19 @@ export function Footer() {
               </svg>
             </Link>
             <Link
-              href="https://twitter.com/vojtechmares_"
+              href="https://x.com/vojtechmares_"
               target="_blank"
               className="group"
-              aria-label="Vojtěch Mareš na Twitter"
+              aria-label="Vojtěch Mareš na X"
             >
               <svg
-                aria-hidden="true"
+                role="img"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
                 className="h-6 w-6 fill-black group-hover:fill-slate-700"
               >
-                <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0 0 22 5.92a8.19 8.19 0 0 1-2.357.646 4.118 4.118 0 0 0 1.804-2.27 8.224 8.224 0 0 1-2.605.996 4.107 4.107 0 0 0-6.993 3.743 11.65 11.65 0 0 1-8.457-4.287 4.106 4.106 0 0 0 1.27 5.477A4.073 4.073 0 0 1 2.8 9.713v.052a4.105 4.105 0 0 0 3.292 4.022 4.093 4.093 0 0 1-1.853.07 4.108 4.108 0 0 0 3.834 2.85A8.233 8.233 0 0 1 2 18.407a11.615 11.615 0 0 0 6.29 1.84" />
+                <title>X</title>
+                <path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z" />
               </svg>
             </Link>
             <Link
