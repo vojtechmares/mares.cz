915be9ec69788c8c8ed1b1f6a586318d1a7f564b
Vojtech Mares <vojtech@mares.cz>
2024-09-01 20:14:25 +0200

    feat: add new TrainingAd component to the end of blog posts

---
+++ components/blog/TrainingAd.tsx
@@ -0,0 +1,58 @@
+import Image, { StaticImageData } from "next/image";
+
+import { Button } from "@/components/Button";
+import { Container } from "@/components/Container";
+
+import { trainingList } from "@/content/traininig-list";
+
+export function TrainingAd({ trainingSlug }: { trainingSlug: string }) {
+  const training = trainingList.find(
+    (training) => training.slug === trainingSlug,
+  );
+
+  if (!training) {
+    return <></>;
+  }
+
+  return (
+    <section className="bg-amber-500 pb-28 pt-20 sm:py-32">
+      <Container>
+        <div className="flex flex-col justify-between sm:flex-row">
+          <div className="max-w-3xl">
+            <h2 className="font-display text-3xl font-bold tracking-tight text-white sm:text-4xl">
+              {training.name} školení
+            </h2>
+            <p className="mt-4 text-lg tracking-tight text-white">
+              {training.adText}
+            </p>
+            <div className="flex flex-col sm:flex-row sm:gap-x-6">
+              <Button
+                href="mailto:vojtech@mares.cz"
+                color="black"
+                size="large"
+                className="mt-10"
+              >
+                Nezávazně poptat firemní školení
+              </Button>
+              <Button
+                href={training.href}
+                color="white"
+                size="large"
+                className="mt-10"
+              >
+                Více informací o školení
+              </Button>
+            </div>
+          </div>
+          <Image
+            src={training.logo.src}
+            alt={training.logo.alt}
+            className="mx-auto rounded-lg p-2 invert"
+            width="196"
+            height="196"
+          />
+        </div>
+      </Container>
+    </section>
+  );
+}
---
+++ content/traininig-list.ts
@@ -0,0 +1,83 @@
+import { StaticImageData } from "next/image";
+
+import logoKubernetes from "@/images/logos/tools/kubernetes.svg";
+import logoTerraform from "@/images/logos/tools/terraform.svg";
+import logoArgo from "@/images/logos/tools/argo.svg";
+import logoGit from "@/images/logos/tools/git.svg";
+import logoPostgres from "@/images/logos/tools/postgresql.svg";
+
+export type TrainingType = {
+  name: string;
+  logo: {
+    src: StaticImageData;
+    alt: string;
+  };
+  price: {
+    public: number;
+    corporate: number;
+  };
+  href: string;
+  slug?: string;
+  days?: number;
+  adText?: string;
+};
+
+export const trainingMap: Record<string, TrainingType> = {};
+
+export const trainingList: TrainingType[] = [
+  {
+    name: "Kubernetes",
+    href: "/skoleni/kubernetes",
+    slug: "kubernetes",
+    logo: {
+      src: logoKubernetes,
+      alt: "Kubernetes logo",
+    },
+    price: { public: 9900, corporate: 44000 },
+    days: 2,
+    adText:
+      "Zajímá Vás Kubernetes a chcete se dozvědět víc? Jak pracovat s Kubernetes, jak spravovat cluster, jak nasadit aplikaci do Kubernetes atd.? Pak přijďte na moje školení o Kubernetes. Školení probíhá formou workshopu, kde si všechno vyzkoušíte na vlastní kůži. Přihlaste se buď na veřejný termín nebo mě kontaktujte ohledně firemního školení.",
+  },
+  {
+    name: "Terraform",
+    href: "/skoleni/terraform",
+    slug: "terraform",
+    logo: {
+      src: logoTerraform,
+      alt: "Terraform logo",
+    },
+    price: { public: 5900, corporate: 24000 },
+    adText:
+      "Pokud Vás zajímá Terraform více do hloubky a chtěli byste se s ním naučit víc nebo chcete aby Váš tým začal Terraform používat, přijďte na moje školení Terraformu. Školení probíhá formou workshopu, kde si práci s Terraformem všichni sami vyzkouší. Buď přijďte na veřejný termín nebo se domluvme na firemním školení.",
+  },
+  {
+    name: "ArgoCD",
+    href: "/skoleni/argocd",
+    slug: "argocd",
+    logo: {
+      src: logoArgo,
+      alt: "ArgoCD logo",
+    },
+    price: { public: 5900, corporate: 24000 },
+  },
+  {
+    name: "Git",
+    href: "/skoleni/git",
+    slug: "git",
+    logo: {
+      src: logoGit,
+      alt: "Git logo",
+    },
+    price: { public: 5900, corporate: 24000 },
+  },
+  {
+    name: "Postgres na Kubernetes",
+    href: "/skoleni/postgres-on-k8s",
+    slug: "postgres-on-k8s",
+    logo: {
+      src: logoPostgres,
+      alt: "PostgreSQL logo",
+    },
+    price: { public: 5900, corporate: 24000 },
+  },
+];
