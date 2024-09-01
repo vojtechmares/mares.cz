c7c61ad04c58a49e5327b9b292690a69943db9ac
Vojtech Mares <vojtech@mares.cz>
2024-09-01 20:26:12 +0200

    refactor(TrainingAd): update responsiveness and make more mobile friendly

--- components/blog/TrainingAd.tsx
+++ components/blog/TrainingAd.tsx
@@ -17,7 +17,7 @@ export function TrainingAd({ trainingSlug }: { trainingSlug: string }) {
   return (
     <section className="bg-amber-500 pb-28 pt-20 sm:py-32">
       <Container>
-        <div className="flex flex-col justify-between sm:flex-row">
+        <div className="flex flex-col justify-between md:flex-row">
           <div className="max-w-3xl">
             <h2 className="font-display text-3xl font-bold tracking-tight text-white sm:text-4xl">
               {training.name} školení
@@ -25,7 +25,7 @@ export function TrainingAd({ trainingSlug }: { trainingSlug: string }) {
             <p className="mt-4 text-lg tracking-tight text-white">
               {training.adText}
             </p>
-            <div className="flex flex-col sm:flex-row sm:gap-x-6">
+            <div className="flex flex-col md:flex-row md:gap-x-6">
               <Button
                 href="mailto:vojtech@mares.cz"
                 color="black"
@@ -47,7 +47,7 @@ export function TrainingAd({ trainingSlug }: { trainingSlug: string }) {
           <Image
             src={training.logo.src}
             alt={training.logo.alt}
-            className="mx-auto rounded-lg p-2 invert"
+            className="mx-auto hidden rounded-lg p-2 invert md:block"
             width="196"
             height="196"
           />
