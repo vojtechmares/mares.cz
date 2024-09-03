de20da0f0cf6bc6bc591e98a39959419c3118036
Vojtech Mares <vojtech@mares.cz>
2024-09-03 11:21:53 +0200

    refactor(blog): scale blog post for readability

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -76,13 +76,15 @@ export default async function Article({ params }: Props) {
   return (
     <>
       <Container>
-        <div className="pb-14 sm:pb-20 lg:pb-32">
-          <div className="prose:text-black prose-h1:font-display prose-h2:font-display prose-h3:font-display prose prose-h1:text-3xl prose-h1:font-extrabold prose-h1:tracking-tight prose-h2:text-2xl prose-h2:font-bold prose-h2:tracking-tight prose-h3:text-xl prose-h3:font-medium prose-p:text-slate-700 prose-li:my-0 prose-h1:sm:text-4xl prose-h2:sm:text-3xl prose-h1:md:text-5xl">
-            <Content />
-          </div>
-        </div>
+        <article className="prose:text-black prose-h1:font-display prose-h2:font-display prose-h3:font-display prose pb-14 md:prose-lg lg:prose-xl prose-h1:text-3xl prose-h1:font-extrabold prose-h1:tracking-tight prose-h2:text-2xl prose-h2:font-bold prose-h2:tracking-tight prose-h3:text-xl prose-h3:font-medium prose-p:text-slate-700 prose-ol:ps-5 prose-ul:ps-5 prose-li:my-0 sm:pb-20 prose-h1:sm:text-4xl prose-h2:sm:text-3xl prose-h1:md:text-5xl prose-ol:md:ps-6 prose-ul:md:ps-6 lg:pb-32">
+          <Content />
+        </article>
       </Container>
-      <TrainingAd trainingSlug={meta.trainingAd} />
+      {meta.trainingAd !== undefined ? (
+        <TrainingAd trainingSlug={meta.trainingAd} />
+      ) : (
+        <></>
+      )}
     </>
   );
 }
