5b780bd8ffce8e588dcb92c0c0b35f791c8009c8
Vojtech Mares <vojtech@mares.cz>
2024-11-03 17:13:12 +0100

    chore(eslint): add jest-dom and testing-library plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
   root: true,
-  extends: ["next", "prettier", "next/core-web-vitals"],
+  extends: [
+    "next",
+    "prettier",
+    "next/core-web-vitals",
+    "plugin:jest-dom/recommended",
+    "plugin:testing-library/react",
+  ],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
