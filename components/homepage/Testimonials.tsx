b9b6b02ee9da55a4e00bc951db52ffe7a345ca1f
Vojtech Mares <vojtech@mares.cz>
2024-11-04 22:32:14 +0100

    feat: add testimonial from Ladislav Capka

--- components/homepage/Testimonials.tsx
+++ components/homepage/Testimonials.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 
 import { Container } from "@/components/Container";
 import avatarAntoninHoskovec from "@/public/images/people/antonin-hoskovec.jpg";
+import avatarLadislavCapka from "@/public/images/people/ladislav-capka.jpg";
 
 const testimonials = [
   [
@@ -12,6 +13,15 @@ const testimonials = [
         name: "Antonín Hoškovec",
         role: "AI team lead, GLAMI",
         image: avatarAntoninHoskovec,
+      },
+    },
+    {
+      content:
+        "S Vojtou Marešem jsme za poslední roky úspěšně zvládli mnoho technických výzev, převážně ve světě Kubernetes, kde se Vojta skvěle orientuje z pohledu operations i vývoje. Společně jsme automatizovali procesy skripty jako GitHub Workflows či GitLab pipelines a řešili komplexní infrastruktury pro veřejné cloudy (AWS, GCP) i vlastní RKE2 clustery s high-availability pokrytím. Vojtovy znalosti sahají i do vývoje – jeho přesah nám umožnil vytvořit základní koncept Kubernetes operátoru v Go, který je klíčovou součástí našeho produktu. Díky své zkušenosti a schopnosti dobře se učit nové věci je pro něj svět Kubernetes otevřenou knihou, a máloco ho zde dokáže překvapit.",
+      author: {
+        name: "Ladislav Čapka",
+        role: "CTO, cybros labs",
+        image: avatarLadislavCapka,
       },
     },
   ],
