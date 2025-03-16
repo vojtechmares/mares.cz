3f4ea9e941dce03a6e1d18eb3c25321d49bdac30
Vojtech Mares <vojtech@mares.cz>
2025-03-16 14:00:33 +0100

    refactor(components): simplify container sizing and add default px-4 padding

--- components/Container.tsx
+++ components/Container.tsx
@@ -10,7 +10,7 @@ export function Container({ className, children }: Props) {
   return (
     <div
       className={clsx(
-        "md:max-5xl mx-auto max-w-sm sm:max-w-3xl sm:px-4 md:px-6 lg:max-w-7xl lg:px-8",
+        "md:max-5xl mx-auto sm:max-w-3xl px-4 md:px-0 lg:max-w-7xl lg:px-8",
         className,
       )}
     >
