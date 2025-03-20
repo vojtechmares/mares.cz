2975fa747ae3a705bdf1329d339ce5a184b0e4b4
Vojtech Mares <vojtech@mares.cz>
2025-03-20 23:15:14 +0100

    feat(components): add landing call-to-action

---
+++ components/landing/call-to-action.tsx
@@ -0,0 +1,15 @@
+import { Container } from "@/components/Container";
+import { Section } from "@/components/ui/section";
+
+export function CallToAction({ id, children }: { id: string, children: React.ReactNode }) {
+  return (
+    <Section
+      id={id}
+      background="amber"
+    >
+      <Container>
+        {children}
+      </Container>
+    </Section>
+  );
+}
