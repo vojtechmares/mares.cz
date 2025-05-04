ba47d5ebaae5819cf0c47cf1db84eacdcfb751b7
Vojtěch Mareš <vojtech@mares.cz>
2025-05-04 15:27:12 +0200

    refactor(app): /[slug] use ISR with 4 hour cache
    Signed-off-by: Vojtěch Mareš <vojtech@mares.cz>

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -27,8 +27,14 @@ export async function generateMetadata(props: {
   };
 }
 
-// SSR
-export const dynamic = "force-dynamic";
+// Next.js will invalidate the cache when a
+// request comes in, at most once every 4 hours.
+export const revalidate = 14_400;
+
+// We'll prerender only the params from `generateStaticParams` at build time.
+// If a request comes in for a path that hasn't been generated,
+// Next.js will server-render the page on-demand.
+export const dynamicParams = true;
 
 export default async function Page(props: { params: Params }) {
   const { slug } = await props.params;
