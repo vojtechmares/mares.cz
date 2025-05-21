4ca8858c0199fef74a960fc278396e2ac42a6097
Vojtěch Mareš <vojtech@mares.cz>
2025-05-21 14:10:48 +0200

    fix(app): wrap analytics script block in react artifact
    Signed-off-by: Vojtěch Mareš <vojtech@mares.cz>

--- app/layout.tsx
+++ app/layout.tsx
@@ -84,13 +84,10 @@ const RootLayout = async ({children}: {children: React.ReactNode}) => {
         />
         <link rel="preconnect" href="https://cdn.mares.cz" />
         {isProduction || suppressGoogleAnalytics ? (
-          <GoogleTagManager gtmId="	GTM-5W5Q3LTV" />
-          {/* Twitter conversion tracking base code */}
-          <Script
-            src="/twpx.js"
-            strategy="lazyOnload"
-          />
-          {/* End Twitter conversion tracking base code */}
+          <>
+            <GoogleTagManager gtmId="	GTM-5W5Q3LTV" />
+            <Script src="/twpx.js" strategy="lazyOnload" />
+          </>
         ) : (
           <></>
         )}
