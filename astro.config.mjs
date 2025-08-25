4e874df77a33bf9a555d11dffbab55575fcfdded
Vojtěch Mareš <vojtech@mares.cz>
2025-08-25 09:49:02 +0200

    chore(deps): add @astro/sitemap
    Signed-off-by: Vojtěch Mareš <vojtech@mares.cz>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
+    site: "https://www.mares.cz",
 
-  integrations: [react()]
+    vite: {
+        plugins: [tailwindcss()],
+    },
+
+    integrations: [react(), sitemap()],
 });
