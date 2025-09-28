2aa5c3b8124153e436aecc4917c29f2b4b7912c2
Vojtěch Mareš <vojtech@mares.cz>
2025-09-29 01:09:08 +0200

    feat: add support for i18n
    Signed-off-by: Vojtěch Mareš <vojtech@mares.cz>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,38 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
-
 import react from "@astrojs/react";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+    output: "server",
     site: "https://www.mares.cz",
+
+    i18n: {
+        locales: ["cs", "en"],
+        defaultLocale: "cs",
+        routing: {
+            prefixDefaultLocale: false,
+        },
+    },
+
+    trailingSlash: "never",
 
     vite: {
         plugins: [tailwindcss()],
     },
 
-    integrations: [react(), sitemap()],
+    integrations: [
+        react(),
+        sitemap({
+            i18n: {
+                defaultLocale: "cs",
+                locales: {
+                    cs: "cs-CZ",
+                    en: "en-US",
+                },
+            },
+        }),
+    ],
 });
