f5740015afafbef2b0db2f1d4041137182167643
Vojtěch Mareš <vojtech@mares.cz>
2025-09-29 11:43:27 +0200

    feat(seo): add basic meta tags, favicon, twitter meta tags, fediverse meta tags
    Signed-off-by: Vojtěch Mareš <vojtech@mares.cz>

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -1,3 +1,24 @@
+export const LocalizedMetadata = [
+    {
+        locale: "cs",
+        title: "Vojtěch Mareš - DevOps architekt",
+        titlePrefix: "Vojtěch Mareš",
+        description:
+            "Vojtěch Mareš - DevOps architekt, konzultant a lektor na volné noze",
+        keywords:
+            "devops, devops achitekt, konzultant, lektor, školení, devops školení, freelancer",
+    },
+    {
+        locale: "en",
+        title: "Vojtěch Mareš - DevOps architect",
+        titlePrefix: "Vojtěch Mareš",
+        description:
+            "Vojtěch Mareš - DevOps architect, consultant, lector, and freelancer",
+        keywords:
+            "devops, devops architect, consultant, lector, training, devops training, freelancer",
+    },
+];
+
 export interface StaticLinkData {
     name: string;
     href: string;
