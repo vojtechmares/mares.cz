eed2278fe7ca9d3faada8eec7d06e763981ac1d6
Vojtěch Mareš <vojtech@mares.cz>
2025-09-29 23:10:14 +0200

    feat: add opengraph image (/:slug/card.png) for custom pages
    Signed-off-by: Vojtěch Mareš <vojtech@mares.cz>

---
+++ src/components/opengraph-images/page.tsx
@@ -0,0 +1,90 @@
+import type { ReactNode } from "react";
+
+function Page({
+    slug,
+    title,
+    description,
+    imageData,
+}: {
+    slug: string;
+    title: string;
+    description: string;
+    imageData: ArrayBuffer;
+}) {
+    return (
+        <div
+            style={{
+                height: "100%",
+                width: "100%",
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center", // flex-end
+                justifyContent: "space-between", // space-between
+                backgroundColor: "white",
+                color: "black",
+            }}
+        >
+            <div
+                style={{
+                    marginLeft: "4rem",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    height: "100%",
+                }}
+            >
+                <p
+                    style={{
+                        fontWeight: 700,
+                        fontSize: "4rem",
+                        marginBottom: 0,
+                        paddingBottom: 0,
+                        color: "#fbbf24",
+                    }}
+                >
+                    {title}
+                </p>
+                <p
+                    style={{
+                        marginTop: "1.5rem",
+                        maxWidth: "32rem",
+                        fontSize: "18px",
+                        lineHeight: "1.5556",
+                    }}
+                >
+                    {description}
+                </p>
+                <p style={{ fontSize: "2rem", fontWeight: 500 }}>
+                    mares.cz/{slug}
+                </p>
+            </div>
+            <img
+                style={{ position: "absolute", bottom: 0, right: 60 }}
+                alt=""
+                height={600}
+                src={imageData as unknown as string}
+            />
+        </div>
+    );
+}
+
+export function CreatePageImageComponent({
+    slug,
+    title,
+    description,
+    imageData,
+}: {
+    slug: string;
+    title: string;
+    description: string;
+    imageData: ArrayBuffer;
+}): ReactNode {
+    return (
+        <Page
+            slug={slug}
+            title={title}
+            description={description}
+            imageData={imageData}
+        />
+    );
+}
---
+++ src/pages/[slug]/card.png.ts
@@ -0,0 +1,57 @@
+import satori from "satori";
+import sharp from "sharp";
+
+import { join } from "node:path";
+import { readFile } from "node:fs/promises";
+
+import { CreatePageImageComponent } from "../../components/opengraph-images/page";
+import { strapi } from "../../lib/strapi";
+
+export async function GET({ params }: { params: { slug: string } }) {
+    const { slug } = params;
+
+    const page = await strapi.getPage(slug);
+
+    const avatarData = await readFile(
+        join(process.cwd(), "./src/images/people/vojtech-mares.png")
+    );
+    const avatarSrc = Uint8Array.from(avatarData).buffer;
+
+    const component = CreatePageImageComponent({
+        slug: page.slug,
+        title: page.title,
+        description: page.description,
+        imageData: avatarSrc,
+    });
+
+    const interFontRegular = await readFile(
+        join(process.cwd(), "./src/fonts/Inter_18pt-Regular.ttf")
+    );
+
+    const svg = await satori(component, {
+        width: 1200,
+        height: 630,
+        fonts: [
+            {
+                name: "Inter",
+                data: interFontRegular,
+                weight: 400,
+            },
+        ],
+    });
+
+    const png = await sharp(Buffer.from(svg)).png().toBuffer();
+    return new Response(png as unknown as ArrayBuffer, {
+        headers: {
+            "Content-Type": "image/png",
+            "Cache-Control": "public, max-age=31536000, immutable",
+        },
+    });
+}
+
+function toArrayBuffer(buffer: Buffer): ArrayBuffer | SharedArrayBuffer {
+    return buffer.buffer.slice(
+        buffer.byteOffset,
+        buffer.byteOffset + buffer.byteLength
+    );
+}
