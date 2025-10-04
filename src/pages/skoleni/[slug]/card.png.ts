b532c6870b48e755af80be56ac729239a3f53bb2
Vojtěch Mareš <vojtech@mares.cz>
2025-10-04 22:27:01 +0200

    feat: add opengraph image for training page
    Signed-off-by: Vojtěch Mareš <vojtech@mares.cz>

---
+++ src/pages/skoleni/[slug]/card.png.ts
@@ -0,0 +1,74 @@
+import satori from "satori";
+import sharp from "sharp";
+
+import { join } from "node:path";
+import { readFile } from "node:fs/promises";
+
+import { CreateTrainingImageComponent } from "../../../components/opengraph-images/training";
+import { strapi } from "../../../lib/strapi";
+
+export async function GET({ params }: { params: { slug: string } }) {
+    const { slug } = params;
+
+    const training = await strapi.getTraining(slug);
+
+    // const avatarData = await readFile(
+    //     join(process.cwd(), "./src/images/people/vojtech-mares.png")
+    // );
+    // const avatarSrc = Uint8Array.from(avatarData).buffer;
+
+    let iconData: ArrayBuffer | string | undefined = undefined;
+    if (training.icon?.url !== undefined) {
+        console.log(training.icon.url);
+        // const res = await fetch(training.icon.url);
+        // iconData = await res.arrayBuffer();
+        iconData = training.icon.url;
+    }
+
+    const component = CreateTrainingImageComponent({
+        slug: training.slug,
+        title: training.title,
+        description: training.description,
+        image: iconData,
+    });
+
+    const interFontRegular = await readFile(
+        join(process.cwd(), "./src/fonts/Inter_18pt-Regular.ttf")
+    );
+    const interFontLight = await readFile(
+        join(process.cwd(), "./src/fonts/Inter_18pt-Light.ttf")
+    );
+    const interFontBold = await readFile(
+        join(process.cwd(), "./src/fonts/Inter_18pt-Bold.ttf")
+    );
+
+    const svg = await satori(component, {
+        width: 1200,
+        height: 630,
+        fonts: [
+            {
+                name: "Inter",
+                data: interFontRegular,
+                weight: 500,
+            },
+            {
+                name: "Inter",
+                data: interFontLight,
+                weight: 300,
+            },
+            {
+                name: "Inter",
+                data: interFontBold,
+                weight: 700,
+            },
+        ],
+    });
+
+    const png = await sharp(Buffer.from(svg)).png().toBuffer();
+    return new Response(png as unknown as ArrayBuffer, {
+        headers: {
+            "Content-Type": "image/png",
+            "Cache-Control": "public, max-age=31536000, immutable",
+        },
+    });
+}
