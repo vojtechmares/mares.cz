3e1d1baae9a07083e26e3b24603a58829c049b05
Vojtěch Mareš <vojtech@mares.cz>
2025-10-05 10:24:50 +0200

    feat: add Google Analytics
    - GoogleAnalytics.astro component
    - env var to disable analytics
    - update layout to include the analytics
    
    Signed-off-by: Vojtěch Mareš <vojtech@mares.cz>

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,4 +4,5 @@ interface ImportMetaEnv {
     readonly STRAPI_API_TOKEN: string;
     readonly NOTION_API_KEY: string;
     readonly NOTION_TRAINING_SESSIONS_DATABASE_ID: string;
+    readonly DISABLE_ANALYTICS: boolean;
 }
