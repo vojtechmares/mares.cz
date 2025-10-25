723b611e75e2a17a9d4555324b6dc22ecda53805
Vojtěch Mareš <vojtech@mares.cz>
2025-10-26 00:58:34 +0200

    refactor: remove astro nodejs adapter
    - will be replaced with cloudflare
    
    Signed-off-by: Vojtěch Mareš <vojtech@mares.cz>

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 
-import node from "@astrojs/node";
-
 // https://astro.build/config
 export default defineConfig({
     output: "server",
@@ -47,8 +45,4 @@ export default defineConfig({
             },
         ],
     },
-
-    adapter: node({
-        mode: "standalone",
-    }),
 });
