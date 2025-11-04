98678f8bdb331e074904db91827de4cda74a698b
Vojtěch Mareš <vojtech@mares.cz>
2025-11-04 22:23:34 +0100

    chore(deps): remove wrangler and astro cloudflare adapter, add astro nodejs adapter
    Signed-off-by: Vojtěch Mareš <vojtech@mares.cz>

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 
-import cloudflare from "@astrojs/cloudflare";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
@@ -48,11 +48,8 @@ export default defineConfig({
         ],
     },
 
-    adapter: cloudflare({
-        imageService: "compile",
-        platformProxy: {
-            enabled: true,
-        },
+    adapter: node({
+        mode: "standalone",
     }),
 
     env: {
