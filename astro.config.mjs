3b9e893d7f768cd4cfb851df10d681a2a006857a
Vojtěch Mareš <vojtech@mares.cz>
2025-12-20 21:32:54 +0100

    refactor(astro): change output mode from server to static
    Signed-off-by: Vojtěch Mareš <vojtech@mares.cz>

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: "static",
   site: "https://www.mares.cz",
 
   redirects: {
