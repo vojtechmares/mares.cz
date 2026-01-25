1b22b619b8166ddc2c850736a170a50c8ca8529c
Vojtěch Mareš <vojtech@mares.cz>
2026-01-25 10:00:27 +0100

    feat: switch to cloudflare image service for astro
    Signed-off-by: Vojtěch Mareš <vojtech@mares.cz>

--- astro.config.mjs
+++ astro.config.mjs
@@ -56,7 +56,7 @@ export default defineConfig({
   },
 
   adapter: cloudflare({
-    imageService: "compile", // transform images with sharp for now
+    imageService: "cloudflare",
   }),
 
   env: {
